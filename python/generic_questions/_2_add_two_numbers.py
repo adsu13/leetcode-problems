c1c69b8fd3f62ed57f62ec692021a9fce7b0a783
Adson Pereira <adsongarapinha@gmail.com>
2025-06-03 23:46:00 -0300

    Update _2_add_two_numbers.py

--- python/generic_questions/_2_add_two_numbers.py
+++ python/generic_questions/_2_add_two_numbers.py
@@ -1,21 +1,31 @@
-class ListNode:
+class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy_head = ListNode(0)
-        current = dummy_head
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        current = dummy
         carry = 0
-        while l1 is not None or l2 is not None or carry != 0:
-            l1_val = l1.val if l1 is not None else 0
-            l2_val = l2.val if l2 is not None else 0
-            total = l1_val + l2_val + carry
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            total = val1 + val2 + carry
             carry = total // 10
             digit = total % 10
+            
             current.next = ListNode(digit)
             current = current.next
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         
-        return dummy_head.next
+        return dummy.next
