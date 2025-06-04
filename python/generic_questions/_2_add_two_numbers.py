ece4afb97318da3c2b0afa7a897564bf5d431599
Adson Pereira <adsongarapinha@gmail.com>
2025-06-03 23:47:17 -0300

    Update _2_add_two_numbers.py

--- python/generic_questions/_2_add_two_numbers.py
+++ python/generic_questions/_2_add_two_numbers.py
@@ -1,15 +1,32 @@
-class ListNode(object):
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    
+    def __repr__(self):
+        return f"ListNode({self.val}, {self.next})"
+    
+    @classmethod
+    def deserialize(cls, data):
+        if not data:
+            return None
+        head = ListNode(data[0])
+        current = head
+        for val in data[1:]:
+            current.next = ListNode(val)
+            current = current.next
+        return head
+    
+    def serialize(self):
+        result = []
+        current = self
+        while current:
+            result.append(current.val)
+            current = current.next
+        return result
 
-class Solution(object):
+class Solution:
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
         dummy = ListNode(0)
         current = dummy
         carry = 0
@@ -28,4 +45,4 @@ class Solution(object):
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
-        return dummy.next
+        return dummy.next.serialize() 
