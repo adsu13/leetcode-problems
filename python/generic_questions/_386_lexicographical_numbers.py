71eef245809e12ef36fdea433ac99ad5b8af44af
Adson Pereira <adsongarapinha@gmail.com>
2025-06-08 19:34:34 -0300

    Update _386_lexicographical_numbers.py

--- python/generic_questions/_386_lexicographical_numbers.py
+++ python/generic_questions/_386_lexicographical_numbers.py
@@ -1,14 +1,15 @@
-def lexicalOrder(n):
-    result = []
-    current = 1
-    for _ in range(n):
-        result.append(current)
-        if current * 10 <= n:
-            current *= 10
-        else:
-            if current >= n:
-                current = current // 10
-            current += 1
-            while current % 10 == 0:
-                current = current // 10
-    return result
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        result = []
+        current = 1
+        for _ in range(n):
+            result.append(current)
+            if current * 10 <= n:
+                current *= 10
+            else:
+                if current >= n:
+                    current = current // 10
+                current += 1
+                while current % 10 == 0:
+                    current = current // 10
+        return result
