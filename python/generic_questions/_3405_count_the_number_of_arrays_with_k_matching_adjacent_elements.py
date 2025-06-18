d817b39680a7fe61536a96e346f8fd4e1963ab6a
Adson Pereira <adsongarapinha@gmail.com>
2025-06-17 21:06:02 -0300

    Update _3405_count_the_number_of_arrays_with_k_matching_adjacent_elements.py

--- python/generic_questions/_3405_count_the_number_of_arrays_with_k_matching_adjacent_elements.py
+++ python/generic_questions/_3405_count_the_number_of_arrays_with_k_matching_adjacent_elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def countArrays(self, n: int, m: int, k: int) -> int:
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
         MOD = 10**9 + 7
         if n == 0:
             return 0
@@ -8,13 +8,14 @@ class Solution:
         prev_dp = [[0] * (m + 1) for _ in range(k + 1)]
         for num in range(1, m + 1):
             prev_dp[0][num] = 1
+        
         for i in range(1, n):
             curr_dp = [[0] * (m + 1) for _ in range(k + 1)]
-            for matches in range(0, k + 1):
+            for matches in range(k + 1):
                 for last in range(1, m + 1):
                     if prev_dp[matches][last] == 0:
                         continue
-                    if matches + 1 <= k:
+                    if matches < k:
                         curr_dp[matches + 1][last] = (curr_dp[matches + 1][last] + prev_dp[matches][last]) % MOD
                     curr_dp[matches][last] = (curr_dp[matches][last] + prev_dp[matches][last] * (m - 1)) % MOD
             prev_dp = curr_dp
